277018f28497234a98a0eda6bdb87628c9a13549
bhubhik <bhubhik@gmail.com>
2022-07-06 19:56:33 +1000

    Manual map fucntion

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ module.exports = {
       fn(arr[i], i);
     }
   },
+  map(arr, fn) {
+    const result = [];
+    for (let i in arr) {
+      result.push(fn(arr[i], i));
+    }
+    return result;
+  },
 };
