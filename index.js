321b7d581f16a6289dd195070bf3dcf367b621d4
bhubhik <bhubhik@gmail.com>
2022-07-06 19:28:57 +1000

    For each function made, yet to be tested

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//Initital empty setup
+//forEach
+
+module.exports = {
+  forEach(arr, fn) {
+    for (let i = 0; i < arr.length; i++) {
+      const value = arr[i];
+      fn(value, i);
+    }
+  },
+};
